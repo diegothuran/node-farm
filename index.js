9ae4adeffaec3582fedc8eecdfad2607d66ce829
Diego Santos <diego.thuran@gmail.com>
2020-11-02 21:50:57 -0300

    adas

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
-const hello = 'Hello word';
-console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn)
+
+const textOut = `this is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
--- read_file.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
-
-cosnt = textOut = 'This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}';
-
-fs.writeFileSync('./txt/Output.txt', textOut);
-console.log('File written');
